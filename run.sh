d2fea9833e13978d4b55cce5c4263132e719c984
jaiwardhan <jaiwardhan_live@hotmail.com>
2021-05-24 21:54:43 +0530

    [Run SCript] Allow cli arguments to pass through to python module

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ PWD_TRIGGER=$(pwd)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "$SCRIPT_DIR"
-python3 "$SCRIPT_DIR/Vax.py"
+python3 "$SCRIPT_DIR/Vax.py" $@
 cd "$PWD_TRIGGER"
